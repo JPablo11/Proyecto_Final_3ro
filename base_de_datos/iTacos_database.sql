af9af6326a29986cbc6288aad2bf94176ac7e816
IsaiasManuelAranda <39062150+IsaiasManuelAranda@users.noreply.github.com>
2018-08-14 23:57:50 -0500

    Update iTacos_database.sql
    Se agregaron  triggers, vistas un poco más complejas y procedimientos almacenados, con su respectivo comentario.

--- base_de_datos/iTacos_database.sql
+++ base_de_datos/iTacos_database.sql
@@ -50,3 +50,88 @@ select nombre_prov, numero_tel_prov from proveedores;
 
 select * from inf_prove;
 
+
+/*Triggers*/
+create trigger salenproductos
+after insert on ventas
+for each row
+update productos set existencia=existencia-new.existence where codigo_produ = new.folio;
+
+insert into productos (codigo_produ, nombre_produ, precio_produ, existencia, contenido, folio_produ) values
+    (1, 'atun pescador', 12.5, 49, 115, 1),
+    (2, 'frijol la granja', 15.2, 50, 22, 2),
+    (3, 'leche de vaca', 10, 200, 11, 3),
+    (4, 'aceite el girasol', 98.5, 99, 12, 4);
+
+select * from productos;
+
+insert into ventas (folio, fecha_venta, producto_venta, existence, precio) values
+    (1, now(), 'atun pescador', 1, 12.5);
+    
+select * from productos;
+/*Este trigger funciona para restarle la cantidad a la existencia del producto cuando uno es vendido.*/
+create trigger aumenta_precio
+after insert on ventas
+for each row
+update productos set precio_produ=precio_produ+new.precio where codigo_produ = new.folio;
+
+insert into ventas (folio, fecha_venta, producto_venta, existence, precio) values
+    (2, now(), 'frijol la granja', 1, 10);
+    
+select * from productos;
+/*Este trigger funciona para aumentarle al precio de un producto.*/
+
+/*Vistas*/
+create view 
+inf_prove as
+select nombre_prov, nombre_produ from proveedores, productos where folio_prov = folio_produ;
+
+select * from inf_prove;
+/*Esta vista muestra la información del proveedor, junto del producto que éste provee.*/
+
+create view
+cantidad_producto as 
+select existencia , nombre_produ, nombre_prov, numero_tel_prov from productos, proveedores where folio_prov = folio_produ;
+
+select * from cantidad_producto; 
+/*Esta vista muestra la existencia de los productos junto con el nombre del proveedor por si se necesita llamar por carencia de éstos.*/
+
+/*Delimitadores*/
+Delimiter //
+create procedure productos_cantidad()
+begin
+declare producto int;
+select count(*) from productos into producto;
+select producto;
+end
+
+//
+Delimiter ;
+
+call productos_cantidad();
+/*Este procedimiento cuenta el número de productos existentes*/
+
+Delimiter //
+create procedure nombre_produ_buscar(
+    in letra char(2),
+    out producto int)
+begin
+select * from productos where nombre_produ like letra;
+select count(*) into producto from productos where nombre_produ like letra;
+end
+//
+Delimiter ;
+
+call nombre_produ_buscar('a%', @cantidad);
+
+select @cantidad;
+
+/*Este procedimiento busca algún producto por la inicial de su nombre.*/
+
+
+
+
+
+
+
+
